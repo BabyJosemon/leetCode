5844ea377fca387109d14fa7971ca90aad71ab15
Josemon Baby <babyjosemon77@gmail.com>
2024-01-30 16:49:10 +0000

    Time: 11 ms (36.24%), Space: 48.9 MB (64.37%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -2,20 +2,17 @@ class Solution {
     public int majorityElement(int[] nums) {
         HashMap<Integer, Integer> cache= new HashMap<Integer, Integer>();
         int answer=0;
+        int maxFrequency=0;//These will be used to get the max in the cache without traversing the cache again
+        int maxFrequencyElement=nums[0];//These will be used to get the max in the cache without traversing the cache again
         for(int i: nums){
-            Integer count = cache.get(i);
-            if(count==null){
-                cache.put(i,1);
-            }else{
-                cache.put(i, count+1);
+            int count = cache.getOrDefault(i, 0) + 1;
+            cache.put(i, count);
+            if(count>maxFrequency){
+                maxFrequency=count;
+                maxFrequencyElement=i;
             }
         }
-        for(int j: cache.keySet()){
-            int count=cache.get(j);
-            if(count>nums.length/2){
-                answer=j;
-            }
-        }
-        return answer;
+        
+        return maxFrequencyElement;
     }
 }
